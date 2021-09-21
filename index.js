ad2aac65c978d6372270369706b748644f4b03a1
Alessandra Chardon <achardon@ucla.edu>
2021-09-20 21:25:30 -0400

    added correct css file, added grocery list

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-console.log('js script working')
+//console.log('js script working')
 
 
-document.querySelector('form').addEventListener('submit', (e) => {
+document.querySelector('#addNotes').addEventListener('submit', (e) => {
     e.preventDefault();
-    const form = document.querySelector('form')
-    console.log(e.target.day.value)
+    const form = document.querySelector('#addNotes')
+    //console.log(e.target.day.value)
     const notes = document.createElement('p')
     notes.innerText = e.target.notes.value
-    const deleteButton = document.createElement('button')
-    deleteButton.innerText = 'x'
-    notes.appendChild(deleteButton)
+    const deleteContent = document.createElement('button')
+    deleteContent.innerText = 'x'
+    notes.appendChild(deleteContent)
     if (e.target.day.value === 'monday') {
         document.querySelector('#mon').appendChild(notes)
     }
@@ -31,11 +31,34 @@ document.querySelector('form').addEventListener('submit', (e) => {
     else if (e.target.day.value === 'sunday') {
         document.querySelector('#sun').appendChild(notes)
     }
-    document.querySelector('button').addEventListener ('click', () => {
-        debugger
-        const parentElement = document.querySelector('button').parentElement
+    deleteContent.addEventListener ('click', () => {
+        //debugger
+        const parentElement = deleteContent.parentElement
         parentElement.remove()
     })
     form.reset()
 })
 
+document.querySelector('#addGroceries').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = document.querySelector('#addGroceries')
+    //console.log(e.target.day.value)
+    const newItem = document.createElement('li')
+    newItem.innerText = e.target.item.value
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'x'
+    newItem.appendChild(deleteButton)
+    document.querySelector('ul').appendChild(newItem)
+    deleteButton.addEventListener ('click', () => {
+        // debugger
+        const parentNode = deleteButton.parentNode
+        parentNode.remove()
+    })
+    form.reset()
+})
+
+
+
+//Questions
+//For the week plan, using parentElement for the delete button works (but not parentNode). 
+//For thte groceries, parentNode does work (and parentElemtn works too)... why the difference?
