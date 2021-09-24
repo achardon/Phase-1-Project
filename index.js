ea245797aa9ac4b403f544de4b9a586021cb499e
Alessandra Chardon <achardon@ucla.edu>
2021-09-24 15:53:59 -0400

    persisted deletions of notes and groceries

--- index.js
+++ index.js
@@ -5,46 +5,25 @@ const week = []
 document.querySelector('#addNotes').addEventListener('submit', (e) => {
     e.preventDefault();
     const form = document.querySelector('#addNotes')
-    //console.log(e.target.day.value)
+    const dayID = e.target.day.value 
+    const note = e.target.notes.value
 
-    //this is where the PATCH fetch request should go for persisting the note to the backend
-    persistNotes(e.target.notes.value, e.target.day.value)
+    //PATCH request
+    persistNotes(note, dayID)
     
-    const notes = document.createElement('p')
-    notes.innerText = e.target.notes.value
-    const deleteContent = document.createElement('button')
-    deleteContent.innerText = 'x'
-    notes.appendChild(deleteContent)
+    // const newNote = document.createElement('p')
+    // newNote.innerText = note
+    // const deleteContent = document.createElement('button')
+    // deleteContent.innerText = 'x'
+    // newNote.appendChild(deleteContent)
 
-    //need to finish below
-    //document.querySelector('#day-${e.target.day.value}').appendChild(notes)
+    // document.querySelector(`#day-${dayID}`).appendChild(newNote)
 
-    if (e.target.day.value === '1') {
-        document.querySelector('#day-1').appendChild(notes)
-    }
-    else if (e.target.day.value === '2') {
-        document.querySelector('#day-2').appendChild(notes)
-    }
-    else if (e.target.day.value === '3') {
-        document.querySelector('#day-3').appendChild(notes)
-    }
-    else if (e.target.day.value === '4') {
-        document.querySelector('#day-4').appendChild(notes)
-    }
-    else if (e.target.day.value === '5') {
-        document.querySelector('#day-5').appendChild(notes)
-    }
-    else if (e.target.day.value === '6') {
-        document.querySelector('#day-6').appendChild(notes)
-    }
-    else if (e.target.day.value === '7') {
-        document.querySelector('#day-7').appendChild(notes)
-    }
-    deleteContent.addEventListener ('click', () => {
-        //debugger
-        const parentElement = deleteContent.parentElement
-        parentElement.remove()
-    })
+    // deleteContent.addEventListener ('click', () => {
+    //     persistDeleteNotes(note, id)
+    //     const parentElement = deleteContent.parentElement
+    //     parentElement.remove()
+    // })
     //debugger;
     form.reset()
 })
@@ -52,22 +31,7 @@ document.querySelector('#addNotes').addEventListener('submit', (e) => {
 document.querySelector('#addGroceries').addEventListener('submit', (e) => {
     e.preventDefault();
     const form = document.querySelector('#addGroceries')
-    //console.log(e.target.day.value)
-    
     persistGroceries(e.target.item.value)
-
-    const newItem = document.createElement('li')
-    newItem.innerText = e.target.item.value
-    const deleteButton = document.createElement('button')
-    deleteButton.innerText = 'x'
-    newItem.appendChild(deleteButton)
-    document.querySelector('ul').appendChild(newItem)
-    deleteButton.addEventListener ('click', () => {
-        // debugger
-        const parentNode = deleteButton.parentNode
-        parentNode.remove()
-        //this is where the fetch function needs to go for deleting the item on the server
-    })
     form.reset()
 })
 
@@ -81,8 +45,6 @@ document.querySelector('#recipeSearch').addEventListener('submit', (e) => {
     document.querySelectorAll('li.ingredient').forEach(li=>li.remove())
     document.querySelectorAll('li.step').forEach(li=>li.remove())
     document.querySelector('#showRecipeIngredients').style.visibility = 'hidden'
-
-    //console.log(searchRequest)
     getRecipes(searchRequest);
 })
 
@@ -103,10 +65,9 @@ function getRecipes(searchRequest) {
             li.addEventListener('click', () => {
                 const recipeNames = document.querySelectorAll('li.search')
                 recipeNames.forEach(recipe => recipe.style.color = 'black')
-                //this line below does not work (recpie stays bolded) - why??
-                recipeNames.forEach(recipe => recipe.style.fontweight = 'normal')
                 li.style.color = 'orange'
                 li.style.fontWeight = 'bold'
+                //remove previously shown recipe info
                 document.querySelectorAll('li.ingredient').forEach(li=>li.remove())
                 document.querySelectorAll('li.step').forEach(li=>li.remove())
                 getRecipeInfo(recipeID)
@@ -116,8 +77,6 @@ function getRecipes(searchRequest) {
 }
 
 function getRecipeInfo(recipeID) {
-    //console.log('inside getRecipeInfo function')
-    //console.log(recipeID)
     fetch(`https://api.spoonacular.com/recipes/${recipeID}/information?apiKey=c19ab73b0fea4182a41a6222b727ccea`)
     .then(res => res.json())
     .then(data => {
@@ -127,13 +86,12 @@ function getRecipeInfo(recipeID) {
         //make section visible
         document.querySelector('#showRecipeIngredients').style.visibility = 'visible'
         document.querySelector('#showRecipeInstructions').style.visibility = 'visible'
-
         //get recipe image
         const image = document.querySelector('img')
         image.src = data.image
         image.alt = 'picture of recipe'
         image.style.height = 20
-        //get reciep ingredients
+        //get recipe ingredients
         data.extendedIngredients.forEach(ingredient => {
             const ingredientInfo = document.createElement('li')
             ingredientInfo.className = 'ingredient'
@@ -142,27 +100,8 @@ function getRecipeInfo(recipeID) {
             ingredientInfo.style.cursor = 'pointer'
             //add event listener to add ingredient to grocery list
             ingredientInfo.addEventListener('click', (e) => {
-
-                //copied from grocery item event listener
-                const form = document.querySelector('#addGroceries')
-
                 persistGroceries(ingredientInfo.innerText)
-
-                const newItem = document.createElement('li')
-                newItem.innerText = ingredientInfo.innerText
-                const deleteButton = document.createElement('button')
-                deleteButton.innerText = 'x'
-                newItem.appendChild(deleteButton)
-                document.querySelector('ul').appendChild(newItem)
-                deleteButton.addEventListener ('click', () => {
-                    // debugger
-                    const parentNode = deleteButton.parentNode
-                    parentNode.remove()
-                })
-                //above copied from grocery item event listener
-
                 ingredientInfo.style.fontStyle = 'italic'
-
             })
         })
         //get recipe instructions
@@ -172,9 +111,7 @@ function getRecipeInfo(recipeID) {
             step.innerText = instruction.step
             document.querySelector('ol#instructions').appendChild(step)
         })
-      
-    })
-    
+    })   
 }
 
 function getGroceries() {
@@ -182,7 +119,6 @@ function getGroceries() {
     .then(res => res.json())
     .then(data => {
         data.forEach(item => {
-            //console.log (item.name)
             const form = document.querySelector('#addGroceries')
             const newItem = document.createElement('li')
             newItem.innerText = item.name
@@ -191,8 +127,7 @@ function getGroceries() {
             newItem.appendChild(deleteButton)
             document.querySelector('ul').appendChild(newItem)
             deleteButton.addEventListener ('click', () => {
-            const parentNode = deleteButton.parentNode
-            parentNode.remove()
+                persistDeleteGroceries(item.id, deleteButton)
             })
         })
     })
@@ -202,46 +137,18 @@ function getNotes() {
     fetch('http://localhost:3000/days')
     .then(res => res.json())
     .then(data => {
-        //addNotes(day.id)
         data.forEach(day => {
             week.push(day)
-            //console.log(day.name, day.notes)
             if (day.notes[0] !== undefined) {
-
-                //debugger;
-                //copied from #addNotes event listener above
-                
                 day.notes.forEach(note => {
-                    
-                    const notes = document.createElement('p')
-                    notes.innerText = note //how to get rid of x?
+                    const newNote = document.createElement('p')
+                    newNote.innerText = note 
                     const deleteContent = document.createElement('button')
                     deleteContent.innerText = 'x'
-                    notes.appendChild(deleteContent)
-                    
-                    if (day.name === 'Monday') {
-                        document.querySelector('#day-1').appendChild(notes)
-                    }
-                    else if (day.name === 'Tuesday') {
-                        document.querySelector('#day-2').appendChild(notes)
-                    }
-                    else if (day.name === 'Wednesday') {
-                        document.querySelector('#day-3').appendChild(notes)
-                    }
-                    else if (day.name === 'Thursday') {
-                        document.querySelector('#day-4').appendChild(notes)
-                    }
-                    else if (day.name === 'Friday') {
-                        document.querySelector('#day-5').appendChild(notes)
-                    }
-                    else if (day.name === 'Saturday') {
-                        document.querySelector('#day-6').appendChild(notes)
-                    }
-                    else if (day.name === 'Sunday') {
-                        document.querySelector('#day-7').appendChild(notes)
-                    }
+                    newNote.appendChild(deleteContent)
+                    document.querySelector(`#day-${day.id}`).appendChild(newNote)
                     deleteContent.addEventListener ('click', () => {
-                        //debugger
+                        persistDeleteNotes(note, day.id)
                         const parentElement = deleteContent.parentElement
                         parentElement.remove()
                     })
@@ -251,9 +158,6 @@ function getNotes() {
     })
 }
 
-//is this supposed to POST or PATCH? I'm getting two errors that don't make sense...
-//this should be a POST
-//once this function works, it needs to be added to event listener for adding groceries to list (AND to event listener for ingredients after a recipe search)
 function persistGroceries(item) {
     fetch('http://localhost:3000/groceries', {
         method: "POST",
@@ -265,7 +169,15 @@ function persistGroceries(item) {
         })
     .then(res => res.json())
     .then(data => {
-        
+        const newItem = document.createElement('li')
+        newItem.innerText = data.name
+        const deleteButton = document.createElement('button')
+        deleteButton.innerText = 'x'
+        newItem.appendChild(deleteButton)
+        document.querySelector('ul').appendChild(newItem)
+        deleteButton.addEventListener ('click', () => {
+            persistDeleteGroceries(data.id, deleteButton)
+        })
     })
 }
 
@@ -284,10 +196,69 @@ function persistNotes(note, id) {
         })
     })
     .then(res => res.json())
-    .then(data => {})
+    .then(data => {
+        const newNote = document.createElement('p')
+        newNote.innerText = note
+        const deleteContent = document.createElement('button')
+        deleteContent.innerText = 'x'
+        newNote.appendChild(deleteContent)
+    
+        document.querySelector(`#day-${id}`).appendChild(newNote)
+    
+        deleteContent.addEventListener ('click', () => {
+            persistDeleteNotes(note, id)
+            const parentElement = deleteContent.parentElement
+            parentElement.remove()
+        })
+    
+    })
 }
 
-//Still need to persist DELETING notes and grocery items. Would that be a DELETE or a PATCH?
+function persistDeleteGroceries(itemID, deleteButton) {
+    fetch(`http://localhost:3000/groceries/${itemID}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const parentNode = deleteButton.parentNode
+        parentNode.remove()
+    })
+}
+
+function persistDeleteNotes(note, id) {
+    console.log(note)
+    console.log(id)
+
+    idIntoInt = parseInt(id) 
+    const day = week.find((day) => (day.id === idIntoInt))
+    //NEED TO DELETE INSTEAD OF ADD NOTE
+    //debugger
+    //const item = day.notes.find(item => (item === note))
+    //item is the item we want to delete, need index of item in array so we can delete that item from the array
+    //can I use findIndex()
+    //day.notes.findIndex(FUNCTION THAT RETURNS ITEM)
+    const index = day.notes.findIndex(x => x === note)
+    //need to remove 'index' declared above from day's notes array
+    day.notes.splice(index, 1)
+    const newArray = day.notes
+    week[idIntoInt - 1].notes = newArray
+    //debugger
+    fetch(`http://localhost:3000/days/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "notes": newArray
+        })
+    })
+    .then(res => res.json())
+    .then(data => {})
+}
 
 getNotes()
 
@@ -295,15 +266,13 @@ getGroceries()
 
 //Questions
 //For the week plan, using parentElement for the delete button works (but not parentNode). 
-//For thte groceries, parentNode does work (and parentElemtn works too)... why the difference?
+//For the groceries, parentNode does work (and parentElemtn works too)... why the difference?
 
 //For getting the notes on the page: I do it with the event listener for the notes form, and also from the json. How would I be able to just make one function called renderNotes when I used e.target.day.value for the first and day.name for the second. How do I make that more streamlined?
 
 //For the json template, there is a section for 'deploying the server' using Heroku. What is that for?
 
 //For some of the recipes, the amount has way too many decimal places (0.333333 cups of oil) - is this easy to fix? (ex: spaghetti squash boats)
-
-//For styling the page, how could I make a block for the grocery list and search functions all in one line? Or have the recipe picture be next to the ingredients and next to the steps?
 
 //after I added the json server and pushed my newest repo to github, it said something about warning: json server something something... can I still push it to github and to the website even though it's running with json?
 
@@ -316,10 +285,3 @@ getGroceries()
 //JSON Notes
 // to run the server in development mode: "npm run dev"
 // Any time you want to reset your database back to your original data, run 'npm run seed' again.
-
-
-
-//Potential things to add
-//line before recipe list: "Click recipe for more info"
-//event listener for ingredient to add to grocery list
-//filter recipes with dietary restrictions
